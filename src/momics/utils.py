1c264ee6d0f36ebb60d0cddac0a3cbdd0b494e04
js2264 <jacquesserizay@gmail.com>
2024-09-26 13:49:29 +0200

    fix: tests

--- src/momics/utils.py
+++ src/momics/utils.py
@@ -48,61 +48,12 @@ def _check_track_names(bw_files, tracks):
             )
 
 
-def _check_chr_name(chr, chroms):
-    if chr not in chroms["chrom"].values:
-        raise ValueError(f"{chr} chromosome not listed in `chroms` table.")
-
-
 def _check_track_name(track, tracks):
     labels = set(tracks["label"])
     if track not in labels:
         raise ValueError(
             f"Provided track name '{track}' does not exist in `tracks` table"
         )
-
-
-def import_bed_file(file_path: Path) -> pd.DataFrame:
-    """Import a local bed file as a DataFrame
-
-    Args:
-        file_path (Path): Path to a local bed file
-
-    Returns:
-        pd.DataFrame: A pd.DataFrame with at least three columns (`chrom`, `start`, `end`),
-        and any other columns stored in the local bed file.
-    """
-    # Convert file_path to a Path object
-    file_path = Path(file_path)
-
-    try:
-        # Read the file into a DataFrame
-        df = pd.read_csv(file_path, sep="\t", header=None)
-
-        # Check if the DataFrame has at least 3 columns
-        if df.shape[1] < 3:
-            print(f"The file {file_path} does not have at least 3 columns.")
-            return False
-
-        # Ensure that columns 2 and 3 are numeric (start and end coordinates)
-        df[1] = pd.to_numeric(
-            df[1], errors="coerce"
-        )  # Convert start column to numeric, set errors to NaN
-        df[2] = pd.to_numeric(
-            df[2], errors="coerce"
-        )  # Convert end column to numeric, set errors to NaN
-
-        if df[1].isna().any() or df[2].isna().any():
-            print(
-                f"Columns 2 and 3 in {file_path} must contain valid numeric "
-                + "values for start and end coordinates."
-            )
-            return False
-
-    except Exception as e:
-        raise e
-
-    df.columns = ["chrom", "start", "end"] + [col for col in df.columns[3:]]
-    return df
 
 
 def parse_ucsc_coordinates(coords: str) -> pd.DataFrame:
